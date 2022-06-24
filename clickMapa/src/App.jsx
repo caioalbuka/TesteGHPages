41d2e323f38cc80906baa7159db284b1d5793539
Caio Albuka <88105753+caioalbuka@users.noreply.github.com>
2022-06-24 13:39:13 -0300

    Delete clickMapa directory

--- clickMapa/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import MapPage from "./pages/MapPage";
-
-export default function App() {
-  return <MapPage />;
-}
--- clickMapa/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://elas-api.mocklab.io/",
-});
-
-export default api;
